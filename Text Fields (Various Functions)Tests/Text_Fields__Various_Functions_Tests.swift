260753cc2e478949d4a35d4b9d50a9e1d9336801
blia4241 <blia4241@uni.sydney.edu.au>
2018-01-01 11:33:27 +0630

    Initial Commit

---
+++ Text Fields (Various Functions)Tests/Text_Fields__Various_Functions_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Text_Fields__Various_Functions_Tests.swift
+//  Text Fields (Various Functions)Tests
+//
+//  Created by Brent Liang on 1/1/18.
+//  Copyright © 2018 Brent Liang. All rights reserved.
+//
+
+import XCTest
+@testable import Text_Fields__Various_Functions_
+
+class Text_Fields__Various_Functions_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
